d6495db40c80adce723972386bc8fb9096f44f58
Liam Tate <mrtea@protonmail.com>
2022-01-07 14:20:07 +0000

    db stuff

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self): #overides a default value of original programming, __str__ can be found in the original file
+        return self.name
